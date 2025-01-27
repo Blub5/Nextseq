70042b55547b2c72d24efe5fa20404d0d6b51ea9
Blub5 <stephanalexw@gmail.com>
2025-01-27 09:07:48 +0100

    test

--- process_form.php
+++ process_form.php
@@ -15,20 +15,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Retrieve POST data
-$project = $_POST['project'];
-$application = $_POST['application'];
-$size = $_POST['size'];
-$coverage = $_POST['coverage'];
-$sampleCount = $_POST['sampleCount'];
-$conc = $_POST['conc'];
-$avgLibSize = $_POST['avgLibSize'];
-$cycli = $_POST['cycli'];
-$clusters = $_POST['clusters'];
-$flowcell = $_POST['%flowcell'];
-$nm = $_POST['nM'];
-$sample_per_flowcell = $_POST['%samplePerP2'];
-$ul_ngs_pool = $_POST['ulNGSPool'];
+// Retrieve POST data with default values
+$project = $_POST['project'] ?? '';
+$application = $_POST['application'] ?? '';
+$size = $_POST['size'] ?? '';
+$coverage = $_POST['coverage'] ?? '';
+$sampleCount = $_POST['sampleCount'] ?? '';
+$conc = $_POST['conc'] ?? '';
+$avgLibSize = $_POST['avgLibSize'] ?? '';
+$cycli = $_POST['cycli'] ?? '';
+$clusters = $_POST['clusters'] ?? '';
+$flowcell = $_POST['flowcell'] ?? '';
+$nm = $_POST['nM'] ?? '';
+$sample_per_flowcell = $_POST['samplePerP2'] ?? '';
+$ul_ngs_pool = $_POST['ulNGSPool'] ?? '';
 
 // Prepare and bind
 $stmt = $conn->prepare("INSERT INTO project_data (project, application, size, coverage, sampleCount, conc, avgLibSize, cycli, clusters, flowcell, nm, sample_per_flowcell, ul_ngs_pool) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
